329b4475777218593819b130922c297671cee252
flasirani <106567343+flasirani@users.noreply.github.com>
2022-06-03 10:08:16 +0200

    Add files via upload

---
+++ meni.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+cout << "     ____   _        ___  "<< endl;
+cout << "    |__  ) (_)      /_ |   "<< endl;
+cout << "       ) |  _ _ ___  | |   "<< endl;
+cout << "      / /  | | '_  ) | |   "<< endl;
+cout << "     / /_  | | | | |_| |_  "<< endl;
+cout << "  __|____|_|_|_|_|_|_____| "<< endl;
+cout << " |_   _/ ____|  __  ) ____|"<< endl;
+cout << "   | || |  __| |__) | |__   "<< endl;
+cout << "   | || | |_ |  __ {|  __|  "<< endl;
+cout << "  _| || |__| | |  | | |___ "<< endl;
+cout << " |_____)_____|_|  |_|_____|" << endl;
+
+cout<< "" <<endl;
+cout<< "        IGRA 1 ZMIJICA" <<endl;
+cout<< "        IGRA 2 VESALICE" <<endl;
+cout<< "        NAPUSTI IGRU" <<endl;
+    return 0;
+}
